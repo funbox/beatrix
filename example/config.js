c78135e18b7a4c3922c8f75a7d2c93040cb22397
igoradamenko <mail@igoradamenko.com>
2020-09-24 18:39:30 +0300

    Add example config and fix build script

---
+++ example/config.js
@@ -0,0 +1,79 @@
+const CHARACTERS = [
+  // space, !, ", #, $, %, &, ', (, ), *, +, comma, -, dot, /,
+  // numbers, :, ;, <, =, >, ?, @, A-Z, [, \, ], ^, _, `, a-z,
+  // {, |, }, ~
+  '0020-007E',
+
+  // non-breaking space
+  '00A0',
+
+  // ©
+  '00A9',
+
+  // «
+  '00AB',
+
+  // »
+  '00BB',
+
+  // soft hyphen
+  '00AD',
+
+  // middle dot (·)
+  '00B7',
+
+  // Ё
+  '0401',
+
+  // А-Я, а-я
+  '0410-044F',
+
+  // ё
+  '0451',
+
+  // en space
+  '2002',
+
+  // em space
+  '2003',
+
+  // thin space
+  '2009',
+
+  // hair space
+  '200A',
+
+  // hyphen (-)
+  '2010',
+
+  // non-breaking hyphen
+  '2011',
+
+  // en dash (–)
+  '2013',
+
+  // em dash (—)
+  '2014',
+
+  // ‘, ’, ‚ (single low-9 quotation mark), ‛, “, ”, „
+  '2018-201E',
+
+  // bullet (•)
+  '2022',
+
+  // ellipsis (…)
+  '2026',
+
+  // narrow non-breaking space
+  '202F',
+
+  // rouble sign (₽)
+  '20BD',
+
+  // minus (−)
+  '2212',
+];
+
+module.exports = {
+  CHARACTERS,
+};
